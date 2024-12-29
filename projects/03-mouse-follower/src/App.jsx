c67fcd8a83ea0d160ce076ff03b988be2cc7a6e5
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-12-29 12:08:36 -0300

    feat: update repo with third project

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,5 +1,66 @@
+import { useEffect, useState } from "react";
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("Componente montado", enabled);
+
+    const handleMove = (e) => {
+      const { clientX, clientY } = e;
+      console.log({ clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    // Cleanup
+    // -> Cuando el componente se desmonta
+    // -> Cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      console.log("Componente desmontado", enabled);
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
+};
+
 function App() {
-  return <h1>Proyecto 3</h1>;
+  const [mounted, setMounted] = useState(true);
+
+  return (
+    <main>
+      {mounted && <FollowMouse />}
+      <button onClick={() => setMounted(!mounted)}>
+        Toggle mounted FollowMouse component
+      </button>
+    </main>
+  );
 }
 
 export default App;
