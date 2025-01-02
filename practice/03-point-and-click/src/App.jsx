56163be51901fcfdffbf401485d6fa877b587586
Emiliano Hotes <emiliano.hotes@despegar.com>
2025-01-02 18:35:06 -0300

    feat: add score

--- practice/03-point-and-click/src/App.jsx
+++ practice/03-point-and-click/src/App.jsx
@@ -1,47 +1,58 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
+const GAME_STATUS = {
+  READY: "ready",
+  PLAYING: "playing",
+  FINISHED: "finished",
+};
+
 function App() {
+  const [gameStatus, setGameStatus] = useState(GAME_STATUS.READY);
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [score, setScore] = useState(0);
 
   const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  useEffect(() => {
-    console.log("Hello");
-    const w = document.body.offsetWidth;
-    const h = document.body.offsetHeight;
-    console.log({ w, h });
-
-    setPosition({
-      x: getRandomNumber(0, w - 50),
-      y: getRandomNumber(0, h - 50),
-    });
-
-    window.addEventListener("resize", () => {
-      console.log("Resized");
-      console.log(window.innerWidth, window.innerHeight);
-    });
-  }, []);
-
-  const handleClick = () => {
+  const clickCircle = () => {
+    setScore(score + 1);
     setPosition({
       x: getRandomNumber(0, window.innerWidth - 50),
       y: getRandomNumber(0, window.innerHeight - 50),
     });
-  }
+  };
+
+  const start = () => {
+    setScore(0);
+    setGameStatus(GAME_STATUS.PLAYING);
+    setPosition({
+      x: getRandomNumber(0, window.innerWidth - 50),
+      y: getRandomNumber(0, window.innerHeight - 50),
+    });
+  };
 
   return (
     <>
-      <div
-      onClick={() => handleClick()}
-        className="square"
-        style={{
-          right: `${position.x}px`,
-          bottom: `${position.y}px`,
-        }}
-      ></div>
+      <div className="scoreCard">
+        <p>score: {score}</p>
+      </div>
+
+      {gameStatus === GAME_STATUS.PLAYING && (
+        <div
+          onClick={() => clickCircle()}
+          className="square"
+          style={{
+            right: `${position.x}px`,
+            bottom: `${position.y}px`,
+          }}
+        />
+      )}
+
+      {gameStatus === GAME_STATUS.READY && (
+        <button onClick={() => start()}>start game</button>
+      )}
     </>
   );
 }
